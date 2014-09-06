da13cf60d2817d174f4c9555b5d56300a843b7b8
Tim Perry <tim.r.perry@gmail.com>
2014-09-06 17:53:50 +0100

    added min menu

--- public_html/content/themes/TimPerry/assets/js/classes/ui.js
+++ public_html/content/themes/TimPerry/assets/js/classes/ui.js
@@ -1,6 +1,29 @@
 var ui = {
 
-    init : function() {
+    init: function () {
+
+        this.toggle_menu.apply(this)
+
+    },
+    toggle_menu: function () {
+
+        var $action_icon = $(".action-icon"),
+            $sidebar = $(".sidebar"),
+            sidebar_minimised_class = "sidebar--minimised";
+
+        $action_icon.click(function () {
+
+            if ($sidebar.hasClass(sidebar_minimised_class)) {
+
+                $sidebar.removeClass(sidebar_minimised_class);
+
+            } else {
+
+                $sidebar.addClass(sidebar_minimised_class);
+
+            }
+
+        });
 
     }
 
--- public_html/content/themes/TimPerry/assets/js/dev.js
+++ public_html/content/themes/TimPerry/assets/js/dev.js
@@ -47,7 +47,30 @@ if (!(window.console && console.log)) {
 })(jQuery);
 var ui = {
 
-    init : function() {
+    init: function () {
+
+        this.toggle_menu.apply(this)
+
+    },
+    toggle_menu: function () {
+
+        var $action_icon = $(".action-icon"),
+            $sidebar = $(".sidebar"),
+            sidebar_minimised_class = "sidebar--minimised";
+
+        $action_icon.click(function () {
+
+            if ($sidebar.hasClass(sidebar_minimised_class)) {
+
+                $sidebar.removeClass(sidebar_minimised_class);
+
+            } else {
+
+                $sidebar.addClass(sidebar_minimised_class);
+
+            }
+
+        });
 
     }
 
